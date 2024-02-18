7a1a481cadb037d28ec524c4d5a445e53b433c69
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-02-18 14:43:56 +0100

    msvc yuck

--- src/util/lowlevel.cpp
+++ src/util/lowlevel.cpp
@@ -20,13 +20,15 @@ namespace util::lowlevel {
 
     // i only did this as an excuse to learn simd :D
     float pcmVolumeFast(const float* pcm, size_t samples) {
-#if defined(GEODE_IS_WINDOWS)
+#if defined(GEODE_IS_WINDOWS) && defined(__clang__)
         size_t alignedSamples = samples / 4 * 4;
 
         __m128 sumVec = _mm_setzero_ps();
+        __m128 maskVec = _mm_castsi128_ps(_mm_set1_epi32(0x7fffffff));
+
         for (size_t i = 0; i < alignedSamples; i += 4) {
             __m128 pcmVec = _mm_loadu_ps(pcm + i);
-            pcmVec = _mm_and_ps(pcmVec, _mm_castsi128_ps(_mm_set1_epi32(0x7fffffff)));
+            pcmVec = _mm_and_ps(pcmVec, maskVec);
             sumVec = _mm_add_ps(sumVec, pcmVec);
         }
 
