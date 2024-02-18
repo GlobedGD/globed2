12a97380262640923dc08cbde93ceb56fa86d9e5
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-02-18 11:27:45 +0100

    migrations todo

--- server/central/migrations/2024-02-17-183931_initial/down.sql
+++ server/central/migrations/2024-02-17-183931_initial/down.sql
@@ -1,1 +1,3 @@
 -- This file should undo anything in `up.sql`
+
+DROP TABLE users;
--- server/central/migrations/2024-02-17-183931_initial/up.sql
+++ server/central/migrations/2024-02-17-183931_initial/up.sql
@@ -1,1 +1,10 @@
 -- Your SQL goes here
+
+CREATE TABLE users (
+    account_id INTEGER PRIMARY KEY,
+    name_color TEXT,
+    user_role SMALLINT,
+    is_banned SMALLINT,
+    is_muted SMALLINT,
+    is_whitelisted SMALLINT,
+);
