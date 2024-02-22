1c65da3c8cfba9788fa3771500c3493aba06c574
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-02-22 20:39:36 +0100

    make user search case-insensitive

--- server/central/src/db/dbimpl.rs
+++ server/central/src/db/dbimpl.rs
@@ -47,8 +47,8 @@ impl GlobedDb {
     }
 
     pub async fn get_user_by_name(&self, name: &str) -> Result<Option<UserEntry>> {
-        let res: Option<UserEntryWrapper> = query_as("SELECT * FROM users WHERE user_name = ?")
-            .bind(name)
+        let res: Option<UserEntryWrapper> = query_as("SELECT * FROM users WHERE user_name LIKE ?")
+            .bind(format!("%{name}%"))
             .fetch_optional(&self.0)
             .await?;
 
