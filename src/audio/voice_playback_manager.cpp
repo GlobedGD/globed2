01953b1650592fff7cab093b68f7d1f08a982bb2
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-02-27 12:04:15 +0100

    omg

--- src/audio/voice_playback_manager.cpp
+++ src/audio/voice_playback_manager.cpp
@@ -109,6 +109,9 @@ void VoicePlaybackManager::forEachStream(std::function<void(int, AudioStream&)> 
 
 #else
 
+#include <util/time.hpp>
+class AudioStream;
+
 void VoicePlaybackManager::playRawDataStreamed(int playerId, const float* pcm, size_t samples) {}
 void VoicePlaybackManager::stopAllStreams() {}
 void VoicePlaybackManager::prepareStream(int playerId) {}
