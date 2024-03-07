352be8871417c89792e12e9302a64df1798da735
dank_meme <42031238+dankmeme01@users.noreply.github.com>
2024-03-07 22:17:26 +0100

    fix msvc being dumb

--- src/data/basic.hpp
+++ src/data/basic.hpp
@@ -3,7 +3,7 @@
 
 // Various macros for data serialization
 
-#define GLOBED_SERIALIZABLE_ENUM(...) BOOST_DESCRIBE_ENUM(__VA_ARGS__)
+#define GLOBED_SERIALIZABLE_ENUM BOOST_DESCRIBE_ENUM
 #define GLOBED_SERIALIZABLE_STRUCT(name, ...) BOOST_DESCRIBE_STRUCT(name, (), __VA_ARGS__)
 #define GLOBED_SERIALIZABLE_CLASS(name, ...) BOOST_DESCRIBE_CLASS(name, __VA_ARGS__)
 #define GLOBED_VERIFY_SERIALIZED_STRUCT(name) static_assert(boost::describe::has_describe_members<name>::value, "struct is not described")
