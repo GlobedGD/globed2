c357c40ca21e56ad5497c0f01a2b32acabab12ee
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-03-23 20:57:21 +0100

    mac momento

--- src/hooks/appdelegate.cpp
+++ src/hooks/appdelegate.cpp
@@ -42,16 +42,27 @@ constexpr ptrdiff_t REAL_LOADING_FINISHED = 0x381310;
 void GlobedAppDelegate::loadingIsFinished() {
     AppDelegate::loadingIsFinished();
 
-    // LoadingLayer::loadingFinished is inlined, so we hook a function that gets called before that (AppDelegate::loadingIsFinished),
-    // nop out the inlined code and replace it with a call to the real loadingFinished
-    // because fuck you that's why
-    static auto* patch = util::lowlevel::call(INLINED_START, REAL_LOADING_FINISHED);
+    // LoadingLayer::loadingFinished is inlined, so we hook a function that gets
+    // called before that (AppDelegate::loadingIsFinished) and patch the inlined code
+
+    // mov rdi, rbx (rbx is LoadingLayer*)
+    static auto* movPatch = util::lowlevel::patch(INLINED_START, {0x48, 0x89, 0xdf});
+    if (!movPatch->isEnabled()) {
+        (void) patch->enable().unwrap();
+    }
+
+    const ptrdiff_t callStart = INLINED_START + movPatch->getBytes().size();
+
+    // add a call to real loadingFinished
+    static auto* patch = util::lowlevel::call(callStart, REAL_LOADING_FINISHED);
     if (!patch->isEnabled()) {
         (void) patch->enable().unwrap();
     }
 
+    const ptrdiff_t nopStart = callStart + patch->getBytes().size();
+
     // nop out the leftovers
-    static auto* nopPatch = util::lowlevel::nop(INLINED_START + patch->getBytes().size(), 3 + 4 + 5 + 3 + 3 + 5);
+    static auto* nopPatch = util::lowlevel::nop(nopStart, 4 + 5 + 3 + 3 + 5);
     if (!nopPatch->isEnabled()) {
         (void) nopPatch->enable().unwrap();
     }
