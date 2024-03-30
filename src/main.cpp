5b0d0fa9f7c5245b769368a9d4e1d8557444fd08
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-03-30 09:12:12 +0100

    make it buildable in debug

--- src/main.cpp
+++ src/main.cpp
@@ -91,17 +91,21 @@ void setupCustomKeybinds() {
         { Keybind::create(KEY_B, Modifier::None) },
         Category::PLAY,
     });
-
 #endif // GLOBED_HAS_KEYBINDS
 }
 
 // just debug printing
 void printDebugInfo() {
     std::string version = Mod::get()->getVersion().toString();
+    unsigned int fv = 0;
+
+#if GLOBED_HAS_FMOD
+    FMODAudioEngine::sharedEngine()->m_system->getVersion(&fv);
+#endif
 
     log::warn("=== Globed {} has been loaded in debug mode ===", version.starts_with('v') ? version : ("v" + version));
     log::info("Platform: {} ({}-endian)", GLOBED_PLATFORM_STRING, GLOBED_LITTLE_ENDIAN ? "little" : "big");
-    log::info("FMOD linkage: {}", GLOBED_HAS_FMOD == 0 ? "false" : "true");
+    log::info("FMOD linkage: {}, version: {:X}", GLOBED_HAS_FMOD == 0 ? "false" : "true", fv);
 #ifdef GLOBED_VOICE_SUPPORT
     log::info("Voice chat support: true (opus version: {})", GlobedAudioManager::getOpusVersion());
 #else
@@ -114,3 +118,9 @@ void printDebugInfo() {
 
     log::info("cURL version: {}, {}", curl_version(), (cvi->features & CURL_VERSION_SSL) ? "with SSL" : "without SSL (!)");
 }
+
+#if defined(_DEBUG) && defined(GEODE_IS_WINDOWS)
+void operator delete(void* block) noexcept {
+    HeapFree(GetProcessHeap(), 0, block);
+}
+#endif
