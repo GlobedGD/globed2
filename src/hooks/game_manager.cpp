9b11628344cffe7f0099055bf3537d12156263df
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-04-01 12:28:06 +0200

    nvm it was a bindings issue

--- src/hooks/game_manager.cpp
+++ src/hooks/game_manager.cpp
@@ -26,7 +26,7 @@ CCTexture2D* HookedGameManager::loadIcon(int iconId, int iconType, int iconReque
         return texture;
     }
 
-    texture = GameManager::loadIcon(iconId, iconType, iconRequestId);
+    texture = GameManager::loadIcon(iconId, iconType, -1);
 
     if (texture) {
         m_fields->iconCache[iconType][iconId] = texture;
