8d68224d069e68f342bbf53a9707f58a076a9e0e
TechStudent10 <76978184+TechStudent10@users.noreply.github.com>
2024-05-31 08:41:24 -0600

    credits server (still need to point to main serv rather than localhost)

--- src/ui/menu/credits/credits_popup.cpp
+++ src/ui/menu/credits/credits_popup.cpp
@@ -1,8 +1,11 @@
 #include "credits_popup.hpp"
+#include <matjson.hpp>
 #include <util/ui.hpp>
 
 #include "credits_player.hpp"
 #include "credits_cell.hpp"
+
+#include <Geode/utils/web.hpp>
 
 using namespace geode::prelude;
 
@@ -39,87 +42,144 @@ bool GlobedCreditsPopup::setup() {
         GlobedCreditsPlayer::create(name, nickname, accountid, userid, Icons { .type = IconType::Cube, .id = cube, .color1 = col1, .color2 = col2, .color3 = col3}) \
     )
 
-    /* Owners */
+    /* Fetch credits from server */
 
-    CCArray* owners = CCArray::create();
+    web::AsyncWebRequest()
+        .fetch("http://127.0.0.1:8080/credits")
+        .json()
+        .then([scrollLayer](matjson::Value const& credits_data) {
+            /* Owners */
 
-    ADD_PLAYER(owners, "dankmeme01", 9735891, 88588343, 1, 41, 16, -1);
-    ADD_PLAYER(owners, "availax", 1621348, 8463710, 373, 6, 52, 52);
+            CCArray* owners = CCArray::create();
+            auto owner_credits = credits_data["owner"].as_array();
 
-    auto* cellOwners = Build<GlobedCreditsCell>::create("Owner", false, owners)
-        .id("cell-owner"_spr)
-        .parent(scrollLayer->m_contentLayer)
-        .collect();
+            for (auto& credit : owner_credits) {
+                ADD_PLAYER_NICK(
+                    owners,
+                    credit["name"].as_string(),
+                    credit["name"].as_string(),
+                    credit["accountID"].as_int(),
+                    credit["userID"].as_int(),
+                    credit["iconID"].as_int(),
+                    credit["color1"].as_int(),
+                    credit["color2"].as_int(),
+                    credit["color3"].as_int());
+            }
 
-    /* Staff */
+            auto* cellOwners = Build<GlobedCreditsCell>::create("Owner", false, owners)
+                .id("cell-owner"_spr)
+                .parent(scrollLayer->m_contentLayer)
+                .collect();
 
-    CCArray* staff = CCArray::create();
+            /* Developer */
 
-    ADD_PLAYER(staff, "Croozington", 18950870, 172707109, 120, 13, 10, 98);
-    ADD_PLAYER(staff, "Xanii", 1176802, 7500092, 4, 19, 12, 12);
+            CCArray* developers = CCArray::create();
+            auto dev_credits = credits_data["dev"].as_array();
 
-    ADD_PLAYER(staff, "ddest1nyy", 1249399, 8533459, 461, 96, 62, 70);
-    ADD_PLAYER(staff, "Dasshu", 1975253, 7855157, 373, 98, 12, 12);
-    ADD_PLAYER(staff, "Lxwnar", 25517837, 153455117, 132, 52, 12, 52);
-    ADD_PLAYER(staff, "BoCKTheBook", 6209270, 20531100, 272, 6, 106, -1);
-    ADD_PLAYER(staff, "ItzKiba", 4569963, 15447500, 110, 6, 10, 42);
-    ADD_PLAYER(staff, "NotTerma", 4706010, 16077956, 379, 98, 36, 43);
-    ADD_PLAYER(staff, "ciyaqil", 13640460, 127054504, 84, 12, 40, 40);
-    ADD_PLAYER(staff, "TheKiroshi", 6442871, 31833361, 1, 8, 11, 63);
-    ADD_PLAYER(staff, "Jiflol", 8262191, 61991331, 135, 44, 12, -1);
-    ADD_PLAYER(staff, "LimeGradient", 7214334, 42454266, 37, 2, 12, 2);
-    ADD_PLAYER(staff, "ManagerMagolor", 25450870, 195501521, 101, 43, 7, -1);
-    ADD_PLAYER(staff, "skrunkly", 14462068, 146500573, 37, 17, 19, -1);
+            for (auto& credit : dev_credits) {
+                ADD_PLAYER_NICK(
+                    developers,
+                    credit["name"].as_string(),
+                    credit["name"].as_string(),
+                    credit["accountID"].as_int(),
+                    credit["userID"].as_int(),
+                    credit["iconID"].as_int(),
+                    credit["color1"].as_int(),
+                    credit["color2"].as_int(),
+                    credit["color3"].as_int());
+            }
 
-    ADD_PLAYER(staff, "Capeling", 18226543, 160504868, 141, 76, 40, 40);
-    ADD_PLAYER_NICK(staff, "Awesomeoverkill", "ninXout", 7479054, 47290058, 77, 1, 5, -1);
-    ADD_PLAYER(staff, "NikoSando", 737532, 4354472, 433, 16, 6, 12);
+            auto* cellDevs = Build<GlobedCreditsCell>::create("Developer", true, developers)
+                .id("cell-developer"_spr)
+                .parent(scrollLayer->m_contentLayer)
+                .collect();            
 
-    auto* cellStaff = Build<GlobedCreditsCell>::create("Staff", true, staff)
-        .id("cell-staff"_spr)
-        .parent(scrollLayer->m_contentLayer)
-        .collect();
+            /* Staff */
 
-    /* Contributor */
+            CCArray* staff = CCArray::create();
+            auto staff_credits = credits_data["staff"].as_array();
 
-    CCArray* contributors = CCArray::create();
+            for (auto& credit : staff_credits) {
+                ADD_PLAYER_NICK(
+                    staff,
+                    credit["name"].as_string(),
+                    credit["name"].as_string(),
+                    credit["accountID"].as_int(),
+                    credit["userID"].as_int(),
+                    credit["iconID"].as_int(),
+                    credit["color1"].as_int(),
+                    credit["color2"].as_int(),
+                    credit["color3"].as_int());
+            }
 
-    ADD_PLAYER_NICK(contributors, "Awesomeoverkill", "ninXout", 7479054, 47290058, 77, 1, 5, -1);
-    ADD_PLAYER(contributors, "Capeling", 18226543, 160504868, 141, 76, 40, 40);
-    ADD_PLAYER(contributors, "TheSillyDoggo", 16778880, 162245660, 5, 41, 40, 12);
-    ADD_PLAYER_NICK(contributors, "angeld233", "angeld23", 28024715, 236381167, 98, 4, 12, 12);
-    ADD_PLAYER(contributors, "Uproxide", 25397826, 227796112, 296, 2, 12, 12);
+            auto* cellStaff = Build<GlobedCreditsCell>::create("Staff", false, staff)
+                .id("cell-staff"_spr)
+                .parent(scrollLayer->m_contentLayer)
+                .collect();
 
-    auto* cellContrib = Build<GlobedCreditsCell>::create("Contributor", false, contributors)
-        .id("cell-contributor"_spr)
-        .parent(scrollLayer->m_contentLayer)
-        .collect();
+            /* Contributor */
 
-    /* Special */
+            CCArray* contributors = CCArray::create();
 
-    CCArray* special = CCArray::create();
+            auto contrib_credits = credits_data["contrib"].as_array();
 
-    ADD_PLAYER(special, "MathieuAR", 3759035, 13781237, 220, 5, 9, 106);
-    ADD_PLAYER(special, "HJfod", 104257, 1817908, 132, 8, 3, 40);
-    ADD_PLAYER(special, "Cvolton", 761691, 6330800, 101, 19, 12, -1);
-    ADD_PLAYER(special, "alk1m123", 11535118, 116938760, 102, 12, 12, -1);
-    ADD_PLAYER(special, "mat4", 5568872, 19127913, 81, 0, 20, 2);
+            for (auto& credit : contrib_credits) {
+                ADD_PLAYER_NICK(
+                    contributors,
+                    credit["name"].as_string(),
+                    credit["name"].as_string(),
+                    credit["accountID"].as_int(),
+                    credit["userID"].as_int(),
+                    credit["iconID"].as_int(),
+                    credit["color1"].as_int(),
+                    credit["color2"].as_int(),
+                    credit["color3"].as_int());
+            }
 
-    auto* cellSpecial = Build<GlobedCreditsCell>::create("Special thanks", true, special)
-        .id("cell-special"_spr)
-        .parent(scrollLayer->m_contentLayer)
-        .collect();
+            auto* cellContrib = Build<GlobedCreditsCell>::create("Contributor", true, contributors)
+                .id("cell-contributor"_spr)
+                .parent(scrollLayer->m_contentLayer)
+                .collect();
 
-    scrollLayer->m_contentLayer->setContentHeight(
-        cellOwners->getScaledContentSize().height
-        + cellStaff->getScaledContentSize().height
-        + cellContrib->getScaledContentSize().height
-        + cellSpecial->getScaledContentSize().height
-    );
-    scrollLayer->m_contentLayer->updateLayout();
+            /* Special */
 
-    // make the list start at the top instead of the bottom
-    util::ui::scrollToTop(scrollLayer);
+            CCArray* special = CCArray::create();
+
+            auto special_credits = credits_data["special"].as_array();
+
+            for (auto& credit : special_credits) {
+                ADD_PLAYER_NICK(
+                    special,
+                    credit["name"].as_string(),
+                    credit["name"].as_string(),
+                    credit["accountID"].as_int(),
+                    credit["userID"].as_int(),
+                    credit["iconID"].as_int(),
+                    credit["color1"].as_int(),
+                    credit["color2"].as_int(),
+                    credit["color3"].as_int());
+            }
+
+            auto* cellSpecial = Build<GlobedCreditsCell>::create("Special thanks", false, special)
+                .id("cell-special"_spr)
+                .parent(scrollLayer->m_contentLayer)
+                .collect();
+
+            scrollLayer->m_contentLayer->setContentHeight(
+                cellOwners->getScaledContentSize().height
+                + cellDevs->getScaledContentSize().height
+                + cellStaff->getScaledContentSize().height
+                + cellContrib->getScaledContentSize().height
+                + cellSpecial->getScaledContentSize().height
+            );
+            scrollLayer->m_contentLayer->updateLayout();
+
+            // make the list start at the top instead of the bottom
+            util::ui::scrollToTop(scrollLayer);
+        })
+        .expect([](std::string const& error) {
+            FLAlertLayer::create("Failed to get credits", fmt::format("{}", error), "OK")->show();
+        });
 
     return true;
 }
