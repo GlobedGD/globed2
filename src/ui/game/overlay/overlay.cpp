c54a34c22c1ee511c7e0ff5ced47f26fbe3ff7ba
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-06-04 18:58:35 +0200

    add version

--- src/ui/game/overlay/overlay.cpp
+++ src/ui/game/overlay/overlay.cpp
@@ -10,16 +10,35 @@ bool GlobedOverlay::init() {
     auto& gs = GlobedSettings::get();
     auto& settings = gs.overlay;
 
-    if (settings.enabled) {
-        Build<CCLabelBMFont>::create("", "bigFont.fnt")
-            .opacity(static_cast<uint8_t>(settings.opacity * 255))
-            .anchorPoint(0.f, 0.f)
-            .store(pingLabel)
-            .parent(this)
-            .id("ping-label"_spr);
-
-        this->setContentSize(pingLabel->getContentSize());
+    if (!settings.enabled) {
+        return true;
     }
+
+    this->setLayout(
+        ColumnLayout::create()
+            ->setAxisReverse(true)
+            ->setAxisAlignment(AxisAlignment::Start)
+            ->setCrossAxisLineAlignment(AxisAlignment::End)
+            ->setGap(3.f)
+    );
+
+    Build<CCLabelBMFont>::create("", "bigFont.fnt")
+        .opacity(static_cast<uint8_t>(settings.opacity * 255))
+        .store(pingLabel)
+        .parent(this)
+        .id("ping-label"_spr);
+
+#ifdef GLOBED_DEBUG
+    std::string versionStr = Mod::get()->getVersion().toVString();
+    Build<CCLabelBMFont>::create(versionStr.c_str(), "bigFont.fnt")
+        .opacity(static_cast<uint8_t>(settings.opacity * 255))
+        .store(versionLabel)
+        .parent(this)
+        .id("version-label"_spr);
+#endif
+
+    this->setContentHeight(CCDirector::get()->getWinSize().height);
+    this->updateLayout();
 
     return true;
 }
@@ -30,8 +49,8 @@ void GlobedOverlay::updatePing(uint32_t ms) {
 
     auto fmted = fmt::format("{} ms", ms);
     pingLabel->setString(fmted.c_str());
-    this->setContentSize(pingLabel->getContentSize());
     this->setVisible(true);
+    this->updateLayout();
 }
 
 void GlobedOverlay::updateWithDisconnected() {
@@ -44,7 +63,7 @@ void GlobedOverlay::updateWithDisconnected() {
     }
 
     pingLabel->setString("Not connected");
-    this->setContentSize(pingLabel->getContentSize());
+    this->updateLayout();
 }
 
 void GlobedOverlay::updateWithEditor() {
@@ -57,7 +76,7 @@ void GlobedOverlay::updateWithEditor() {
     }
 
     pingLabel->setString("N/A (Local level)");
-    this->setContentSize(pingLabel->getContentSize());
+    this->updateLayout();
 }
 
 GlobedOverlay* GlobedOverlay::create() {
