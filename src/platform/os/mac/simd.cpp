0d080dbbf8581b095c6d2ca947409aea88f4a4ef
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-06-21 16:40:43 +0200

    fix mac simd and cmakelits

--- src/platform/os/mac/simd.cpp
+++ src/platform/os/mac/simd.cpp
@@ -1,8 +1,18 @@
+#include <Geode/platform/cplatform.h>
+
 #include <util/simd.hpp>
 #include <util/misc.hpp>
 
-#include <platform/arch/x86/x86simd.hpp>
+#ifdef GEODE_IS_ARM_MAC
+# include <platform/arch/arm/armsimd.hpp>
+#else
+# include <platform/arch/x86/x86simd.hpp>
+#endif
 
 float util::simd::calcPcmVolume(const float *pcm, size_t samples) {
+#ifdef GEODE_IS_ARM_MAC
+    return globed::simd::arm::pcmVolume(pcm, samples);
+#else
     return globed::simd::x86::pcmVolume(pcm, samples);
+#endif
 }
