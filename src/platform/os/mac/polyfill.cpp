103589fae71b5d39af5e56ad938fa2527380e4d0
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-06-28 21:26:38 +0200

    small fixes

--- src/platform/os/mac/polyfill.cpp
+++ src/platform/os/mac/polyfill.cpp
@@ -1,4 +1,5 @@
 #include <platform/basic.hpp>
+#include <cocos2d.h>
 
 void cocos2d::CCScheduler::scheduleSelector(SEL_SCHEDULE pfnSelector, CCObject *pTarget, float fInterval, bool bPaused) {
     this->scheduleSelector(pfnSelector, pTarget, fInterval, kCCRepeatForever, 0.f, bPaused);
