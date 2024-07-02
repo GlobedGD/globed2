6ec72f9e4baa802f261aa6d674c87a927dc32af4
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-07-03 00:23:47 +0200

    fix level appearing at the bottom of saved list

--- src/ui/menu/room/download_level_popup.cpp
+++ src/ui/menu/room/download_level_popup.cpp
@@ -54,6 +54,30 @@ void DownloadLevelPopup::loadLevelsFailed(char const* p0) {
 void DownloadLevelPopup::setupPageInfo(gd::string p0, char const* p1) {}
 
 void DownloadLevelPopup::levelDownloadFinished(GJGameLevel* level) {
+    // thank you cvolton :D
+    // this is needed so the level appears at the top of the saved list (unless Manual Level Order is enabled)
+
+    auto* levels = GameLevelManager::get()->m_onlineLevels;
+
+    bool putAtLowest = GameManager::get()->getGameVariable("0084");
+
+    int idx = 0;
+    for (const auto& [k, level] : CCDictionaryExt<gd::string, GJGameLevel*>(levels)) {
+        if (putAtLowest) {
+            idx = std::min(idx, level->m_levelIndex);
+        } else {
+            idx = std::max(idx, level->m_levelIndex);
+        }
+    }
+
+    if (putAtLowest) {
+        idx -= 1;
+    } else {
+        idx += 1;
+    }
+
+    level->m_levelIndex = idx;
+
     this->onClose(this);
     util::ui::switchToScene(LevelInfoLayer::create(level, false));
 }
