cf3e417eed35fa22346396405313256ea56324f0
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-07-29 09:42:05 +0200

    fast reset for everyone off

--- src/util/gd.hpp
+++ src/util/gd.hpp
@@ -10,7 +10,14 @@ namespace util::gd {
     void openProfile(int accountId, int userId, const std::string& name);
     int calcLevelDifficulty(GJGameLevel* level);
 
-
     int getIconWithType(const PlayerIconData& data, PlayerIconType type);
     int getIconWithType(const PlayerIconData& data, IconType type);
+
+    enum class GameVariable {
+        FastRespawn = 52,
+        FastMenu = 168,
+    };
+
+    bool variable(GameVariable var);
+    void setVariable(GameVariable var, bool state);
 }
