eb71365eaa19a5ffcb2f573059ec790caa144e42
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-09-24 15:37:32 +0200

    remove vtable_cast

--- src/util/lowlevel.hpp
+++ src/util/lowlevel.hpp
@@ -69,46 +69,4 @@ namespace util::lowlevel {
 
         return reinterpret_cast<Out*>(ptr);
     }
-
-    // Cast a polymorphic object to another (potentially unrelated by inheritance) polymorphic object.
-    // Advantages: significantly faster than dynamic_cast or typeinfo_cast (simply compares main vtable pointer)
-    // Disadvantages:
-    // * will only work if `object` is an exact instance of `To`, will not work if `To` is a parent class
-    // * will only work if `To` has a bound constructor
-    template <typename To, typename From>
-    requires std::is_pointer_v<To> && std::is_polymorphic_v<std::remove_pointer_t<To>> && std::is_polymorphic_v<From>
-    To vtable_cast(From* object) {
-        if (!object) return nullptr;
-        using Inst = typename std::remove_pointer_t<To>;
-
-        Inst obj;
-        return vtable_cast_with_obj(object, &obj);
-    }
-
-    // Like `vtable_cast` but lifts the requirement of `To` having a default constructor.
-    // You must provide an instance of `To`
-    template <typename To, typename From>
-    requires std::is_pointer_v<To> && std::is_polymorphic_v<std::remove_pointer_t<To>> && std::is_polymorphic_v<From>
-    To vtable_cast_with_obj(From* object, To toInst) {
-        if (!object || !toInst) return nullptr;
-
-        void** vtableTo = *reinterpret_cast<void***>(toInst);
-
-        return vtable_cast_with_table<To>(object, vtableTo);
-    }
-
-    // Like `vtable_cast` but you provide the vtable pointer yourself.
-    template <typename To, typename From>
-    requires std::is_pointer_v<To> && std::is_polymorphic_v<std::remove_pointer_t<To>> && std::is_polymorphic_v<From>
-    To vtable_cast_with_table(From* object, void* vtableTo) {
-        if (!object || !vtableTo) return nullptr;
-
-        void** vtableFrom = *reinterpret_cast<void***>(object);
-
-        if (vtableFrom == vtableTo) {
-            return reinterpret_cast<To*>(object);
-        }
-
-        return nullptr;
-    }
 }
