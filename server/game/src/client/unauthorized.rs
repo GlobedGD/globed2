c93caa6697f9a618cb1ed7c18363307453bca8b7
larzie <lunar@lavache.com>
2024-11-28 18:24:02 +0300

    Update unauthorized.rs

--- server/game/src/client/unauthorized.rs
+++ server/game/src/client/unauthorized.rs
@@ -81,7 +81,7 @@ impl UnauthorizedThread {
             socket: LockfreeMutCell::new(ClientSocket::new(socket, peer, 0, game_server)),
             connection_state: AtomicClientThreadState::default(),
 
-            secret_key: rand::thread_rng().random(),
+            secret_key: rand::rng().random(),
             protocol_version: AtomicU16::new(0),
 
             account_id: AtomicI32::new(0),
