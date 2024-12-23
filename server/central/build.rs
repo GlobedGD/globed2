dd0f6f91af3af9d754b395c4efad4867cafb33fb
larzie <lunar@lavache.com>
2024-12-23 16:16:54 +0300

    Update build.rs

--- server/central/build.rs
+++ server/central/build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    // trigger recompilation when a new migration is added
+    // Trigger recompilation when a new migration is added
     println!("cargo:rerun-if-changed=migrations");
 }
