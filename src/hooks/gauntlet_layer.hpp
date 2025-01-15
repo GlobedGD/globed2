170217964cd26ac49935585c2e0bac8430a65f3c
LimeGradient <87611796+LimeGradient@users.noreply.github.com>
2025-01-15 15:27:06 -0500

    gauntlet level player counts :3

---
+++ src/hooks/gauntlet_layer.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <defs/geode.hpp>
+
+#include <Geode/modify/GauntletLayer.hpp>
+
+struct GLOBED_DLL HookedGauntletLayer : geode::Modify<HookedGauntletLayer, GauntletLayer> {
+    struct Fields {
+        std::unordered_map<int, uint16_t> levels;
+        std::unordered_map<int, CCNode*> wrappers;
+    };
+
+    $override
+    bool init(GauntletType type);
+
+    $override
+    void loadLevelsFinished(cocos2d::CCArray* p0, char const* p1, int p2);
+
+    void buildUI();
+    void refreshPlayerCounts();
+    void updatePlayerCounts(float);
+};
