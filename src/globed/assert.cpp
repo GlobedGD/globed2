51fb3e0c336f05fb3487f7b0ba7f743d6db91b91
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-03-01 02:40:46 +0100

    fix stacktrace again..

--- src/globed/assert.cpp
+++ src/globed/assert.cpp
@@ -6,62 +6,13 @@ using namespace geode::prelude;
 
 #ifdef GLOBED_DEBUG
 
-#include <cpptrace/cpptrace.hpp>
-
-struct Module {
-    std::string name;
-    uintptr_t base;
-};
-
-# ifdef GEODE_IS_WINDOWS
-
-// this code is very borrowed from geode
-static HMODULE handleFromAddress(void const* addr) {
-    HMODULE module = nullptr;
-    GetModuleHandleEx(
-        GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
-        (LPCTSTR)addr, &module
-    );
-    return module;
-}
-
-static std::string getModuleName(HMODULE module, bool fullPath = true) {
-    char buffer[MAX_PATH];
-    if (!GetModuleFileNameA(module, buffer, MAX_PATH)) {
-        return "<Unknown>";
-    }
-    if (fullPath) {
-        return buffer;
-    }
-    return std::filesystem::path(buffer).filename().string();
-}
-
-template <typename T>
-static std::optional<Module> moduleFromAddress(T addr_) {
-    auto addr = reinterpret_cast<const void*>(addr_);
-    auto handle = handleFromAddress(addr);
-    if (!handle) {
-        return std::nullopt;
-    }
-
-    Module mod;
-
-    mod.base = reinterpret_cast<uintptr_t>(handle);
-    mod.name = getModuleName(handle);
-
-    return mod;
-}
-
-# else
-template <typename T>
-static std::optional<Module> moduleFromAddress(T addr_) {
-    return std::nullopt;
-}
-#endif
+# include <cpptrace/cpptrace.hpp>
 
 static void printStacktrace(const cpptrace::stacktrace& trace) {
     log::debug("\n{}", trace.to_string(true));
 }
+
+#endif
 
 [[noreturn]] void globed::_condFail(const std::source_location& loc, std::string_view message) {
     throw std::runtime_error(_condFailSafe(loc, message));
@@ -87,5 +38,3 @@ std::string globed::_condFailSafe(const std::source_location& loc, std::string_v
 
     util::debug::suicide();
 }
-
-#endif
