a590b1e7454904e7dd83c25618cfb6452aa0481c
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-05-13 15:58:28 +0200

    try ios fingerprint impl

--- src/platform/os/ios/misc.cpp
+++ src/platform/os/ios/misc.cpp
@@ -1,8 +1,41 @@
 #include <defs/minimal_geode.hpp>
+
 #include <util/misc.hpp>
+#include <util/crypto.hpp>
+
+#include <CoreFoundation/CoreFoundation.h>
+#include <IOKit/IOKitLib.h>
 
 using util::misc::UniqueIdent;
 
+static std::string getUuid() {
+    io_registry_entry_t ioRegistryRoot = IORegistryEntryFromPath(kIOMasterPortDefault, "IOService:/");
+    CFStringRef uuidCf = (CFStringRef)IORegistryEntryCreateCFProperty(ioRegistryRoot, CFSTR(kIOPlatformUUIDKey), kCFAllocatorDefault, 0);
+    IOObjectRelease(ioRegistryRoot);
+
+    if (!uuidCf) {
+        return "";
+    }
+
+    char uuid[256];
+    if (CFStringGetCString(uuidCf, uuid, sizeof(uuid), kCFStringEncodingUTF8)) {
+        CFRelease(uuidCf);
+        return std::string(uuid);
+    }
+
+    CFRelease(uuidCf);
+    return "";
+}
+
 Result<UniqueIdent> util::misc::fingerprintImpl() {
-    return Err("Unimplemented");
+    auto uuid = getUuid();
+    if (uuid.empty()) {
+        return Err("Failed to get UUID");
+    }
+
+    auto hashed = util::crypto::simpleHash(fmt::format("{}-{}", uuid, Mod::get()->getID()));
+    std::array<uint8_t, 32> arr;
+    std::copy_n(hashed.data(), 32, arr.begin());
+
+    return Ok(UniqueIdent(arr));
 }
