1f0a8d11162496786177983ec3f5fd0909018738
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-05-23 23:47:18 +0200

    update

--- src/hooks/all.hpp
+++ src/hooks/all.hpp
@@ -13,7 +13,6 @@
 #include "level_select_layer.hpp"
 #include "loading_layer.hpp"
 #include "menu_layer.hpp"
-#include "more_options_layer.hpp"
 #include "pause_layer.hpp"
 #include "play_layer.hpp"
 #include "player_object.hpp"
