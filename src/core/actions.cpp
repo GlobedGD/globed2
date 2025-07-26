338f5e246f0074eea82cf046084a229d718802d6
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-07-27 01:28:52 +0200

    impl openUserProfile

--- src/core/actions.cpp
+++ src/core/actions.cpp
@@ -1,4 +1,5 @@
 #include <globed/core/actions.hpp>
+#include <globed/util/singleton.hpp>
 
 using namespace geode::prelude;
 
@@ -7,7 +8,14 @@ namespace globed {
 void warpToSession(SessionId session) {}
 void warpToLevel(int level) {}
 
-void openUserProfile(int accountId, int userId, std::string_view username) {}
+void openUserProfile(int accountId, int userId, std::string_view username) {
+    bool myself = accountId == cachedSingleton<GJAccountManager>()->m_accountID;
 
+    if (!myself) {
+        cachedSingleton<GameLevelManager>()->storeUserName(userId, accountId, gd::string(username));
+    }
+
+    ProfilePage::create(accountId, myself)->show();
+}
 
 }
