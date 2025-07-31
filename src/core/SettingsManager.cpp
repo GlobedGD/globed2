9a7c167c451396eafd940f652fe4c671cd5bb813
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-07-31 15:30:03 +0200

    add command line overrides for settings

--- src/core/SettingsManager.cpp
+++ src/core/SettingsManager.cpp
@@ -99,6 +99,60 @@ void SettingsManager::freeze() {
     m_frozen = true;
 }
 
+static std::optional<matjson::Value> findOverride(std::string_view key) {
+    static auto overrideMap = []{
+        std::unordered_map<std::string, matjson::Value> map;
+        auto names = Loader::get()->getLaunchArgumentNames();
+
+        for (auto& name : names) {
+            if (name.starts_with("globed/")) {
+                auto val = *Loader::get()->getLaunchArgument(name);
+
+                // if there's no value, assume it is a bool flag
+                if (val.empty()) {
+                    map[name.substr(7)] = true;
+                    continue;
+                }
+
+                auto res = matjson::parse(val);
+
+                if (res) {
+                    map[name.substr(7)] = *res;
+                } else {
+                    log::error("Failed to parse launch argument override '{}': {}", name, res.unwrapErr());
+                }
+            }
+        }
+
+        // Print all the overrides
+        for (const auto& [k, v] : map) {
+            log::debug("Launch argument override: {} = {}", k, v.dump(matjson::NO_INDENTATION));
+        }
+
+        return map;
+    }();
+
+    auto it = overrideMap.find(std::string(key));
+    if (it != overrideMap.end()) {
+        auto& value = it->second;
+        return value;
+    }
+
+    return std::nullopt;
+}
+
+static std::string_view matjsonTypeToStr(matjson::Type t) {
+    switch (t) {
+        case matjson::Type::Null: return "null";
+        case matjson::Type::Bool: return "bool";
+        case matjson::Type::Number: return "number";
+        case matjson::Type::String: return "string";
+        case matjson::Type::Object: return "object";
+        case matjson::Type::Array: return "array";
+        default: return "unknown";
+    }
+}
+
 void SettingsManager::registerSetting(
     std::string_view key,
     std::string_view name,
@@ -120,16 +174,35 @@ void SettingsManager::registerSetting(
     m_defaults[hash] = defaultVal;
     m_fullKeys[hash] = fullKey;
 
-    if (auto val = ValueManager::get().getValueRaw(fullKey)) {
+    auto tryApply = [&](const std::optional<matjson::Value>& val, std::string_view fromWhere) {
+        if (!val) return false;
+
         if (val->type() != defaultVal.type()) {
-            log::error("Type mismatch for setting {}, using default value", key);
-            m_settings[hash] = defaultVal;
-            return;
+            log::error(
+                "Type mismatch for setting {} loaded from {}, expected type '{}' but got '{}'",
+                key, fromWhere,
+                matjsonTypeToStr(defaultVal.type()), matjsonTypeToStr(val->type())
+            );
+
+            return false;
         }
 
         m_settings[hash] = *val;
-    } else {
-        m_settings[hash] = defaultVal;
+        return true;
+    };
+
+    // try overrides first
+    if (tryApply(findOverride(fullKey), "launch args")) {
+        return;
+    }
+
+    if (tryApply(ValueManager::get().getValueRaw(fullKey), "savefile")) {
+        return;
+    }
+
+    if (!tryApply(defaultVal, "default")) {
+        // this should never happen!
+        throw std::runtime_error(fmt::format("failed to apply default value for setting {}", key));
     }
 }
 
