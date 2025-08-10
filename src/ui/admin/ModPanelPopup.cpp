a036ccf4339e23ebdc813a0ddfdf01bce69c64bf
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-08-10 17:11:20 +0200

    make the mod panel popup slightly better

--- src/ui/admin/ModPanelPopup.cpp
+++ src/ui/admin/ModPanelPopup.cpp
@@ -1,5 +1,6 @@
 #include "ModPanelPopup.hpp"
 #include "ModNoticeSetupPopup.hpp"
+#include "ModAuditLogPopup.hpp"
 
 #include <UIBuilder.hpp>
 
@@ -7,16 +8,51 @@ using namespace geode::prelude;
 
 namespace globed {
 
-const CCSize ModPanelPopup::POPUP_SIZE { 300.f, 160.f };
+const CCSize ModPanelPopup::POPUP_SIZE { 300.f, 150.f };
 
 bool ModPanelPopup::setup() {
-    Build<ButtonSprite>::create("Hello", "bigFont.fnt", "GJ_button_01.png", 0.7f)
+    auto playerContainer = Build<CCMenu>::create()
+        .layout(RowLayout::create()->setAutoScale(false))
+        .anchorPoint(0.5f, 0.5f)
+        .contentSize(270.f, 30.f)
+        .pos(this->fromCenter(0.f, 30.f))
+        .parent(m_mainLayer)
+        .collect();
+
+    Build<TextInput>::create(200.f, "Username / ID", "chatFont.fnt")
+        .parent(playerContainer);
+
+    Build<CCSprite>::createSpriteName("GJ_longBtn05_001.png")
+        .scale(0.9f)
+        .parent(playerContainer);
+
+    playerContainer->updateLayout();
+
+    auto btnContainer = Build<CCMenu>::create()
+        .layout(RowLayout::create())
+        .anchorPoint(0.5f, 0.5f)
+        .contentSize(270.f, 35.f)
+        .pos(this->fromCenter(0.f, -10.f))
+        .parent(m_mainLayer)
+        .collect();
+
+    Build<ButtonSprite>::create("Notice", "bigFont.fnt", "GJ_button_01.png", 0.7f)
+        .scale(0.9f)
         .intoMenuItem([this] {
             ModNoticeSetupPopup::create()->show();
         })
         .scaleMult(1.1f)
-        .pos(this->center())
-        .parent(m_buttonMenu);
+        .parent(btnContainer);
+
+    Build<ButtonSprite>::create("Logs", "bigFont.fnt", "GJ_button_01.png", 0.7f)
+        .scale(0.9f)
+        .intoMenuItem([this] {
+            ModAuditLogPopup::create()->show();
+        })
+        .scaleMult(1.1f)
+        .parent(btnContainer);
+
+    btnContainer->updateLayout();
 
     return true;
 }
