a5f1d8b0fcc280af829bf7bbccfde774ae1a1173
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-10-23 17:47:23 +0200

    add object string builder

--- src/modules/scripting-ui/hooks/GameManager.cpp
+++ src/modules/scripting-ui/hooks/GameManager.cpp
@@ -8,6 +8,31 @@
 
 using namespace geode::prelude;
 
+struct ObjectStringBuilder {
+    ObjectStringBuilder& prop(int key, auto&& value) {
+        if (!_inner.empty()) _inner.push_back(',');
+        _inner += fmt::format("{},{}", key, value);
+        return *this;
+    }
+
+#define osgetter(g, id) ObjectStringBuilder& g(int value) { return prop(id, value); }
+    osgetter(objectId, 1);
+    osgetter(x, 2);
+    osgetter(y, 3);
+#undef osgetter
+
+    gd::string build() {
+        _inner.push_back(';');
+#ifdef GEODE_IS_ANDROID
+        return _inner;
+#else
+        return std::move(_inner);
+#endif
+    }
+
+    std::string _inner;
+};
+
 struct GLOBED_MODIFY_ATTR SCGameManager : Modify<SCGameManager, GameManager> {
     static void onModify(auto& self) {
         GLOBED_CLAIM_HOOKS(globed::ScriptingUIModule::get(), self,
@@ -22,21 +47,30 @@ struct GLOBED_MODIFY_ATTR SCGameManager : Modify<SCGameManager, GameManager> {
 
         switch (type) {
             case None: {
-                auto str = GameManager::stringForCustomObject(id);
-#ifdef GLOBED_DEBUG
-                log::debug("Object string: '{}'", str);
-#endif
-                return str;
+                return GameManager::stringForCustomObject(id);
             } break;
 
-            // TODO: improve this, dont hardcode strings
             case FireServer: {
                 // -1846870015 is the mask thing
-                return "1,3620,2,525,3,75,36,1,476,-16777216,479,1,483,1,482,-1846870015;";
+                return ObjectStringBuilder{}
+                    .objectId(3620)
+                    .x(525)
+                    .y(75)
+                    .prop(36, 1)
+                    .prop(476, -16777216)
+                    .prop(479, 1)
+                    .prop(483, 1)
+                    .prop(482, -1846870015)
+                    .build();
             } break;
 
             case EmbeddedScript: {
-                return "1,914,2,585,3,105,31,R0xPQkVEX1NDUklQVADEGXv6IwAAACi1L_0gIxkBAAAKAHNjcmlwdC5sdWERAAAALS0gWW91ciBjb2RlIGhlcmUA;";
+                return ObjectStringBuilder{}
+                    .objectId(914)
+                    .x(585)
+                    .y(105)
+                    .prop(31, "R0xPQkVEX1NDUklQVADEGXv6IwAAACi1L_0gIxkBAAAKAHNjcmlwdC5sdWERAAAALS0gWW91ciBjb2RlIGhlcmUA")
+                    .build();
             } break;
 
             default: break;
