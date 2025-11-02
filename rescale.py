4eb2ed0482e89e14cb3a2edc9abe6fa178be3452
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-11-02 22:15:42 +0100

    rescale all emotes

--- rescale.py
+++ rescale.py
@@ -6,13 +6,24 @@ from pathlib import Path
 def rescale(src: Path, dst: Path, px: int):
     print(f"Rescaling {src} to {px} pixels wide")
 
-    proc = Popen(["ffmpeg", "-i", str(src.resolve()), "-y", "-vf", f"scale={px}:-1", str(dst.resolve())], stdout=PIPE, stderr=PIPE)
+    used_temp = None
+    if dst == src:
+        used_temp = "/tmp/rescale_temp.png"
+
+    used_dest = used_temp if used_temp else str(dst)
+
+    proc = Popen(["ffmpeg", "-i", str(src.resolve()), "-y", "-vf", f"scale={px}:-1", used_dest], stdout=PIPE, stderr=PIPE)
     if proc.wait(1.0) != 0:
         print(f"Failed to rescale {src}")
         if proc.stdout: print(proc.stdout.read())
         if proc.stderr: print(proc.stderr.read())
+        return
+
+    if used_temp:
+        dst.write_bytes(Path(used_temp).read_bytes())
 
 gv2 = Path("resources/_raw/globedV2")
+esprites = Path("resources/emotes_sprites")
 
 small = [
     "exit01.png",
@@ -39,9 +50,14 @@ large = [
     "moonSendBtn.png",
 ]
 
-for s in small:
-    rescale(gv2 / s, Path("resources/menu") / s, 128)
+# for s in small:
+#     rescale(gv2 / s, Path("resources/menu") / s, 128)
 
-for s in large:
-    rescale(gv2 / s, Path("resources/menu") / s, 256)
+# for s in large:
+#     rescale(gv2 / s, Path("resources/menu") / s, 256)
 
+for s in esprites.iterdir():
+    if s.suffix.lower() not in [".png", ".jpg", ".jpeg"]:
+        continue
+
+    rescale(s, s, 128)
