de51961e44032f1342efd1712144b419b6bf246d
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-11-23 19:27:54 +0100

    add debug print

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -15,3 +15,15 @@ Result<> Core::addModule(std::shared_ptr<Module> mod) {
 }
 
 }
+
+#ifdef GLOBED_DEBUG
+$on_mod(Loaded) {
+    log::info("===========================================");
+    log::info("Globed loaded in debug mode");
+    log::info("Globed commit: {}, Geode commit: {}", globed::constant<"globed-commit">(), globed::constant<"geode-commit">());
+    log::info("Build date: {} (imprecise)", globed::constant<"build-time">());
+    log::info("Build environment: {}", globed::constant<"build-env">());
+    log::info("Build options: {}", globed::constant<"build-opts">());
+    log::info("===========================================");
+}
+#endif
