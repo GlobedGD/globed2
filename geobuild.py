93ccaf0f61c4dd8dfc2e31396dc886e2d9098bee
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-12-13 16:01:13 +0100

    bump qunet

--- geobuild.py
+++ geobuild.py
@@ -18,7 +18,7 @@ import tomllib
 
 # minimum required geode, can be a commit or a tag
 REQUIRED_GEODE_VERSION = "v4.10.0"
-QUNET_VERSION = "a99a508"
+QUNET_VERSION = "4181104"
 SERVER_SHARED_VERSION = "49d7eab"
 CUE_VERSION = "233549d"
 
