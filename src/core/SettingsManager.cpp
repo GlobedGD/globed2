a73d0a02ff3180831fae289b137ed3feb2d170ea
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2026-02-20 22:46:45 +0100

    make defer disabled by default

--- src/core/SettingsManager.cpp
+++ src/core/SettingsManager.cpp
@@ -15,7 +15,7 @@ SettingsManager::SettingsManager() {
 
     // Preload
     this->registerSetting("core.preload.enabled", true);
-    this->registerSetting("core.preload.defer", true);
+    this->registerSetting("core.preload.defer", false);
 
     // Various
     this->registerSetting("core.autoconnect", true);
