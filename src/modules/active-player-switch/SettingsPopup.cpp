7fa35f95e4bd596c066a85079a450692291b36f3
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2026-02-21 01:44:53 +0100

    incomplete settings ui

--- src/modules/active-player-switch/SettingsPopup.cpp
+++ src/modules/active-player-switch/SettingsPopup.cpp
@@ -3,6 +3,7 @@
 #include <ui/Core.hpp>
 
 #include <cue/LoadingCircle.hpp>
+#include <cue/Util.hpp>
 
 using namespace geode::prelude;
 
@@ -24,6 +25,53 @@ bool APSSettingsPopup::init(APSPlayLayer* layer) {
         .pos(this->fromBottom(40.f))
         .parent(m_buttonMenu)
         .collect();
+
+    // left side - radio buttons for algorithm
+    auto llayout = Build(ColumnContainer::create())
+        .parent(m_mainLayer)
+        .pos(this->fromCenter(-100.f, 20.f))
+        .collect();
+
+    std::array<std::pair<const char*, APSSelectionAlgorithm>, 3> algos = {
+        std::make_pair("Random", APSSelectionAlgorithm::Random),
+        std::make_pair("Fair Random", APSSelectionAlgorithm::FairRandom),
+        std::make_pair("Sequential", APSSelectionAlgorithm::Sequential),
+    };
+
+    for (auto& [name, algo] : algos) {}
+
+    // right side - sliders for interval, var, warning delay
+
+    auto rlayout = Build(ColumnContainer::create())
+        .parent(m_mainLayer)
+        .pos(this->fromCenter(100.f, 20.f))
+        .collect();
+
+    std::array<std::pair<const char*, float*>, 3> sliders = {
+        std::make_pair("Switch Interval", &m_settings.m_interval),
+        std::make_pair("Switch Variance", &m_settings.m_intervalVar),
+        std::make_pair("Warning Delay", &m_settings.m_warningDelay),
+    };
+
+    for (auto& [name, field] : sliders) {
+        auto cont = Build<ColumnContainer>::create()
+            .parent(rlayout)
+            .collect();
+
+        Build(createSlider())
+            .with([&] (cue::Slider* slider) {
+                slider->setCallback([this, field](cue::Slider*, double value) {
+                    *field = value;
+                    m_dirty = true;
+                });
+            })
+            .parent(cont);
+
+        cont->updateLayout();
+    }
+
+    rlayout->updateLayout();
+    cue::attachBackground(rlayout);
 
     return true;
 }
