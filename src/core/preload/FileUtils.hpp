e6c6e54be4f6e6615e54a967497a32a7abccbfc1
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2026-03-17 17:56:29 +0100

    fix android fr now

--- src/core/preload/FileUtils.hpp
+++ src/core/preload/FileUtils.hpp
@@ -75,9 +75,11 @@ struct HookedFileUtils : public cocos2d::CCFileUtils {
 
         return (attrs != INVALID_FILE_ATTRIBUTES &&
                 !(attrs & FILE_ATTRIBUTE_DIRECTORY));
-#else
+#elif !defined(GEODE_IS_ANDROID)
         struct stat st;
         return (stat(path, &st) == 0) && S_ISREG(st.st_mode);
+#else
+        return CCFileUtils::get()->isFileExist(path); // it isn't that slow since it only checks the zip file and doesnt fopen
 #endif
     }
 
