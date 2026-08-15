0a8ec476fa0a1179b8cfd521acbfde1cb9e6400e
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2026-08-15 20:25:45 +0200

    alias fixes

--- include/globed/aliases.hpp
+++ include/globed/aliases.hpp
@@ -1,12 +1,34 @@
 #pragma once
 
 #include <Geode/Result.hpp>
-#include <Geode/utils/function.hpp>
 #include <Geode/utils/ZStringView.hpp>
+#include <Geode/utils/function.hpp>
 #include <cocos2d.h>
-#include <cocos-ext.h>
 
 /// Aliases for omitting `geode::` and `cocos2d::` in code
+
+namespace geode {
+
+template <typename T>
+class Ref;
+template <typename T>
+class WeakRef;
+
+class Label;
+class Button;
+class NineSlice;
+
+namespace cocos {
+    class CCMenuItemExt;
+}
+
+namespace log {}
+
+}
+
+namespace cocos2d::extension {
+    class CCScale9Sprite;
+}
 
 namespace globed {
 
@@ -15,12 +37,16 @@ using Result = geode::Result<T, E>;
 
 using geode::Ok;
 using geode::Err;
-using geode::Ref;
-using geode::Label;
 using geode::ZStringView;
 using geode::Function;
 using geode::FunctionRef;
+
+using geode::Ref;
+using geode::WeakRef;
+using geode::Label;
 using geode::Button;
+using geode::NineSlice;
+using geode::cocos::CCMenuItemExt;
 using cocos2d::CCPoint;
 using cocos2d::CCSize;
 using cocos2d::CCNode;
